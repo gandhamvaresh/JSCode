b68a4fc1707338ade2cc8f573bc2f598a7abaed0
varesh <gandhamvaresh@gmail.com>
2022-01-22 22:11:47 +0530

    Update sumCountOfArray.js

--- sumCountOfArray.js
+++ sumCountOfArray.js
@@ -2,6 +2,7 @@ let arr = [10, 20, 20, 10, 10, 30, 50, 10, 20];
 // Sum count of an array values
 
 let sumCount = arr.reduce((x,y)=>x+y);
+//  multiply also works same manner 
 
 console.log(sumCount);
 
